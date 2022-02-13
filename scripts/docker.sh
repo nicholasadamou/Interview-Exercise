b547ff99264ce55cbb102071ecb82b5378803259
Nicholas Adamou <nicholasadamouemail@gmail.com>
2022-02-13 11:56:36 -0500

    Update docker.sh

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -2,6 +2,8 @@
 
 PROJECTS=('database' 'person-service' 'front-end')
 
+docker network create "interview"
+
 for project in "${PROJECTS[@]}"; do
 	cd "$project" && {
 		make all && docker-compose up -d
